f4c347c3a9756e14eec26574f4fa9750cf8248da
Khushi H N <khushinanjesh25@gmail.com>
2026-08-11 23:09:55 +0530

    Add explanation for regex in isPalindrome method
    Added comment to explain the regex replacement for special characters.

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        String r=s.replaceAll("[^a-zA-Z0-9]","");
+        String r=s.replaceAll("[^a-zA-Z0-9]",""); //This line used to ignore all special characters from String (:,; etc) becuse its helps to compare each character with another 
 
         int low=0;
         int high=r.length()-1;
