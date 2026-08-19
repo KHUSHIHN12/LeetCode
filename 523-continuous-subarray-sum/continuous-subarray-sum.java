d673012e2b4d29673a507135db3db5219637f190
Khushi H N <khushinanjesh25@gmail.com>
2026-08-19 23:35:40 +0530

    Add explanation for prefix sum initialization
    Added comment explaining the purpose of the initial map entry for prefix sum.

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -4,7 +4,9 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         int sum=0;
         int reminder=0;
-        map.put(0,-1);
+        map.put(0,-1);  /*map.put(0, -1)` means that a prefix sum of `0` exists before the array starts, and it helps calculate 
+                          the exact number of elements in a subarray that starts from index `0`*/
+
 
         for(int i=0;i<nums.length;i++)
         {
